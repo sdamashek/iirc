244915f00faef0bb5e499767cf3bd34f1c7906ce
Vacation9 <samuel.damashek@gmail.com>
2014-01-04 21:08:40 -0500

    License change, remove non-init files

--- iirc/__init__.py
+++ iirc/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (C) 2013 Samuel Damashek
+# Copyright (C) 2013 Samuel Damashek, Fox Wilson, James Forcier
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -14,4 +14,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
